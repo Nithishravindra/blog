de4121b20bdfa77dfd968a371187836a682b65c8
nithish ravindra <nithishravindra8@gmail.com>
2020-09-07 12:22:39 +0530

    removed duplicate client files

--- src/components/layout/Layout.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "../../styles/layout.module.css";
-
-export default class layout extends React.Component {
-  render() {
-    return <div className={classes.layout}>{this.props.children}</div>;
-  }
-}
